e10c8284a8ee5c77eeaf2690110b4eeaa1b0f808
ANDREW ILIJIC <ailijic@gmail.com>
2023-08-16 15:03:05 -0500

    Create main.c

---
+++ main.c
@@ -0,0 +1,12 @@
+// Copyright (C) 2023 Andrew N. Ilijic - All Rights Reserved
+// You may use, distribute and modify this code under the
+// terms of the BSD Zero Clause (0BSD) license.
+// 
+// You should have received a copy of the BSD Zero Clause (0BSD) license with
+// this file. If not, please write to: ailijic@gmail.com, or visit: https://choosealicense.com/licenses/0bsd/
+
+#include <stdio.h>
+
+int main(void) {
+  puts("Hello, Human.");
+}
